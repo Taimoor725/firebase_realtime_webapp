c4fd670fda77d7657c1dfd84bc6c894dfd19851e
moor <taimoor725tanveer@gmail.com>
2024-11-27 16:44:19 +0500

    abc

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import SensorReadings from "../components/SensorReadings";
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Sensor Data Viewer</h1>
+      <h1>Welcome to Sensor Values Viewer</h1>
       <SensorReadings />
     </div>
   );
