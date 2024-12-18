24c06121be20a9f587dba81fc7181d91748ea922
moor <taimoor725tanveer@gmail.com>
2024-12-18 13:02:40 +0500

    hasseb pic remove

--- app/group/page.js
+++ app/group/page.js
@@ -6,11 +6,11 @@ function page() {
     <div className='flex flex-col relative w-[80%] h-full z-0'>
         {/* <div>Respected Mambers of Egale Group</div> */}
 
-        <div className='absolute flex flex-col bottom-10 -right-[20rem] justify-center items-center'>
+        {/* <div className='absolute flex flex-col bottom-10 -right-[20rem] justify-center items-center'>
             <div>Khasosi Tavun</div>
             <Image src={'/hasseb.jpg'} height={150}  alt="abdullah" width={150} className=' object-cover w-20 h-20 rounded-full'/>
 
-        </div>
+        </div> */}
     <div className='grid grid-cols-2 ml-[20%] gap-10 p-20 space-x-28 w-full h-full'>
         <div className='flex flex-col items-center justify-center mt-10'>
             <Image src={'/WhatsApp Image 2024-12-18 at 10.16.18_778533f9.jpg'} height={600} width={600} className='w-[350px] h-[350px] rounded-full object-cover'/>
