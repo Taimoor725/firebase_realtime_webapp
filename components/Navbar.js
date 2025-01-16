f2c58e6bcf74b0252bd2cf5e6ad0e2879cd0c937
high-haseeb <haseebkhalidoriginal@gmail.com>
2025-01-16 06:47:57 +0500

    updated the landing page

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,27 +3,18 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <nav className="w-screen top-0 h-20 bg-black flex items-center justify-between px-6 shadow-lg z-10 font-bold">
-      {/* Logo Section */}
+    <div className="w-screen h-auto py-1 flex items-center justify-between px-6 z-10 font-semibold fixed top-0 left-0 text-black">
+
       <div className="text-white text-2xl font-bold">
-        <Image src={"/logo/uet-lahore-seeklogo.svg"} height={600} width={600} className="w-24 h-24"/>
+        <Image src={"/logo/uet-lahore-seeklogo.svg"} height={600} width={600} className="w-24 h-24" alt="uet-logo" />
       </div>
 
-      {/* Navigation Links */}
-      <div className="flex space-x-6 pr-6">
-        <a href="#home" className="text-white hover:text-#9BA8B8-200 text-2xl hover:bg-[#9BA8B8] p-2 rounded-md">
-          Abstract
-        </a>
-        <a href="#about" className="text-white hover:text-#9BA8B8-200 text-2xl p-2 rounded-md hover:bg-[#9BA8B8]">
-          Readings
-        </a>
-        <a href="#services" className="text-white hover:text-#9BA8B8-200 text-2xl p-2 rounded-md hover:bg-[#9BA8B8]">
-          Graphs
-        </a>
-        <a href="#contact" className="text-white hover:text-#9BA8B8-200 text-2xl p-2 rounded-md hover:bg-[#9BA8B8]">
-          Members
-        </a>
+      <div className="flex gap-4 pr-6 text-lg">
+        <a href="#home" className="rounded-md"> Abstract </a>
+        <a href="#about" className="rounded-md"> Readings </a>
+        <a href="#services" className="rounded-md"> Graphs </a>
+        <a href="#contact" className="rounded-md"> Members </a>
       </div>
-    </nav>
+    </div>
   );
 }
